ec7c195a609d17705d36c4bcb18915f6523f800b
Vipin <vn.nawal02@gmail.com>
2025-12-18 01:51:49 +0530

    Fix DATABASE_URL configuration for Supabase

--- gymshalaMngSys/settings.py
+++ gymshalaMngSys/settings.py
@@ -68,8 +68,8 @@ WSGI_APPLICATION = 'gymshalaMngSys.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True,
     )
