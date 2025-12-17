db75c4eeb475dbef9cc7800ea28de8f38b0f4ade
Vipin <vn.nawal02@gmail.com>
2025-12-18 01:32:46 +0530

    Fix SECRET_KEY configuration

--- gymshalaMngSys/settings.py
+++ gymshalaMngSys/settings.py
@@ -10,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hb-r8#h0^6t58w=ex!ico!*^g0(fld#@p%5=jw_-9qzmz$af7d'
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
